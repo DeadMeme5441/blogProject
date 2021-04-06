b6434fb810b99104b01a6ca752beb0b3c3ebae0f
DeadUnderscoreMeme <deadunderscorememe@gmail.com>
2021-04-06 23:44:34 +0530

    removed gitignore

---
+++ client/src/CommentList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+const CommentList = ({ comments }) => {
+
+    const renderedComments = comments.map(comment => {
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content
+        }
+        if (comment.status === 'rejected') {
+            content = 'This comment was rejected by moderation.'
+        }
+        if (comment.status === 'pending') {
+            content = 'This comment is awaiting moderation'
+        }
+        return <li key={comment.id}>{content}</li>
+    })
+
+    return <ul>
+        {renderedComments}
+    </ul>
+}
+
+export default CommentList
